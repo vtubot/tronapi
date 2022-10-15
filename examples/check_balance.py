faf5f12de0a86d9ebf4efbf39fd0f35662fccfbb
vtubot <96474730+vtubot@users.noreply.github.com>
2022-10-15 20:43:25 +0100

    Create check_balance.py

---
+++ examples/check_balance.py
@@ -0,0 +1,15 @@
+from tronpy import Tron
+from tronpy.exceptions import AddressNotFound
+from pprint import pprint
+
+client = Tron()
+
+def check_balance(address):
+    try:
+        balance=client.get_account_balance(address)
+        return balance
+    except AddressNotFound:
+        return 'Adress not found..!'
+
+
+pprint(check_balance('<address>'))
